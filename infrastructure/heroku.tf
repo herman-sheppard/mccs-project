c1b43ae60b67c1f86f7cc8face7e9d351fd84b7b
Herman Sheppard <sheppard.herman@gmail.com>
2022-04-08 11:07:50 +0200

    #2 - Add terraform config

---
+++ infrastructure/heroku.tf
@@ -0,0 +1,37 @@
+ resource "heroku_app" "mccs_staging" {
+   name   = "mccs-staging"
+   region = "eu"
+
+   buildpacks = [
+     "heroku/gradle"
+   ]
+ }
+
+ resource "heroku_addon" "mccs_staging_db" {
+   app  = heroku_app.mccs_staging.id
+   plan = "heroku-postgresql:hobby-dev"
+ }
+
+ resource "heroku_pipeline" "mccs_pipeline" {
+   name = "mccs-build-pipeline"
+ }
+
+ resource "heroku_pipeline_coupling" "staging_pipeline_coupling" {
+   app      = heroku_app.mccs_staging.id
+   pipeline = heroku_pipeline.mccs_pipeline.id
+   stage    = "staging"
+ }
+
+ resource "herokux_pipeline_github_integration" "pipeline_integration" {
+   pipeline_id = heroku_pipeline.mccs_pipeline.id
+   org_repo = "herman-sheppard/mccs"
+ }
+
+ resource "herokux_app_github_integration" "mccs_gh_integration" {
+   app_id = heroku_app.mccs_staging.uuid
+   branch = "master"
+   auto_deploy = true
+   wait_for_ci = true
+
+   depends_on = [herokux_pipeline_github_integration.pipeline_integration]
+ }
---
+++ infrastructure/terraform.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  required_providers {
+    heroku = {
+      source = "heroku/heroku"
+      version = "4.8.0"
+    }
+
+    herokux = {
+      source = "davidji99/herokux"
+      version = "0.33.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "herman-sheppard"
+
+    workspaces {
+      name = "mccs-project"
+    }
+  }
+
+  required_version = "1.1.7"
+}
+
+provider "heroku" {
+  email = var.heroku_email
+  api_key = var.heroku_api_key
+}
+
+provider "herokux" {
+  api_key = var.heroku_api_key
+}
