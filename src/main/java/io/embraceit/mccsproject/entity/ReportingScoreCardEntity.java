9bd88503c50583d320fc63c51fd9db17d3922019
Herman Sheppard <sheppard.herman@gmail.com>
2022-04-09 04:25:48 +0200

    Add entities

---
+++ src/main/java/io/embraceit/mccsproject/entity/ReportingScoreCardEntity.java
@@ -0,0 +1,10 @@
+package io.embraceit.mccsproject.entity;
+
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Embeddable;
+
+@JmixEntity(name = "mccs_ReportingScoreCardEntity")
+@Embeddable
+public class ReportingScoreCardEntity {
+}
---
+++ src/main/java/io/embraceit/mccsproject/entity/ReportingTableOfContentEntity.java
@@ -0,0 +1,10 @@
+package io.embraceit.mccsproject.entity;
+
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Embeddable;
+
+@JmixEntity(name = "mccs_ReportingTableOfContentEntity")
+@Embeddable
+public class ReportingTableOfContentEntity {
+}
