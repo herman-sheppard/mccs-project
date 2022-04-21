d7e7c154fe426820e8f3cb2b81f319eb57ab98a7
Herman Sheppard <sheppard.herman@gmail.com>
2022-04-21 09:30:57 +0200

    Add logo widget

---
+++ src/main/java/io/embraceit/mccsproject/screen/widget/logowidget/LogoWidget.java
@@ -0,0 +1,12 @@
+package io.embraceit.mccsproject.screen.widget.logowidget;
+
+import io.jmix.dashboardsui.annotation.DashboardWidget;
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("mccs_LogoWidget")
+@UiDescriptor("logo-widget.xml")
+@DashboardWidget(name = "Logo")
+public class LogoWidget extends ScreenFragment {
+}
